881e94044a5fefcfb53e90f208d221023655dff0
Anton Agafonov <otlichnonick@yandex.ru>
2021-07-28 14:19:35 +0300

    Initial Commit

---
+++ PHPickerTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PHPickerTest
+//
+//  Created by Anton on 28.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PHPickerTest/PHPickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHPickerTestApp.swift
+//  PHPickerTest
+//
+//  Created by Anton on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct PHPickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
